52654167f3c7665d44718bf7c4ae38948fecbabb
evgeniisolonitsyn <evgensolon@icloud.com>
2022-07-30 13:30:16 +0300

    Homework-6 complete

---
+++ Task41/Program.cs
@@ -0,0 +1,26 @@
+﻿int size = InputInt("Введите размер массива: ");
+int[] array = new int[size];
+
+for(int i = 0; i < size; i++)
+{
+    Console.Write("Введите {0} число: ", i + 1);
+    array[i] = int.Parse(Console.ReadLine());
+}
+
+int positiveNumbers = 0;
+
+for(int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        positiveNumbers++;
+    }
+}
+Console.WriteLine(positiveNumbers);
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+    
+}
---
+++ Task43/Program.cs
@@ -0,0 +1,18 @@
+﻿double b1 = InputDouble("Введите координаты b1: ");
+double k1 = InputDouble("Введите координаты k1: ");
+
+double b2 = InputDouble("Введите координаты b2: ");
+double k2 = InputDouble("Введите координаты k2: ");
+
+double x = -(b1 - b2)/(k1 - k2);
+double y = k1 * x + b1;
+// double y = (b1 - b2)/(k1 - k2);
+
+Console.WriteLine($"({x};{y})");
+
+double InputDouble(string output)
+{
+    Console.Write(output);
+    return Convert.ToDouble(Console.ReadLine());
+    
+}
